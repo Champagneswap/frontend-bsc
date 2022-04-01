e086e9bbda60935623dae13cb92465026ba9ca84
web3 <jq@zencoin.is>
2022-04-01 09:57:08 +0200

    list update
    list

--- src/config/constants/lists.ts
+++ src/config/constants/lists.ts
@@ -1,13 +1,19 @@
 
 const CHAMPAGNE_EXTENDED = 'https://tokens.champagneswap.com/champagneswap-extended.json'
 const CHAMPAGNE_TOP100 = 'https://tokens.champagneswap.com/champagneswap-top-100.json'
+const COINGECKO = 'https://tokens.pancakeswap.finance/coingecko.json'
+const CMC = 'https://tokens.pancakeswap.finance/cmc.json'
 
 export const UNSUPPORTED_LIST_URLS: string[] = []
+
+export const OFFICIAL_LISTS = [CHAMPAGNE_EXTENDED, CHAMPAGNE_TOP100]
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   CHAMPAGNE_TOP100,
   CHAMPAGNE_EXTENDED,
+  COINGECKO,
+  CMC,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
